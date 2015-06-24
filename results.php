bae494772e0072501c15b298688446b2a0fb3c1a
Litsa Litsa <litsa.litsa@bazaarvoice.com>
2015-06-24 15:45:45 -0500

    Added <body> back.  Whoops!

--- results.php
+++ results.php
@@ -17,6 +17,7 @@
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
+        <body style="padding: 0;">
         
         <div class="results">
 
