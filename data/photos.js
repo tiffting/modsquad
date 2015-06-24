36d07798033e6d9774b942c89bf4ae50d6a4e787
Tiff Ting <tiff.ting@gmail.com>
2015-06-23 20:35:00 -0500

    Added more foundation to slick

--- data/photos.js
+++ data/photos.js
@@ -36,105 +36,105 @@
 			"nickname" : "rickthedick24" 
 		},
 		{
-			"id" : "6",
+			"id" : "1",
 			"group" : "b",
 			"src" : "photo-6.jpg",
 			"comment" : "#nationalflagday #merica #coorslight #spyoptics #underarmour #bisonreservoir #yeticoolers #goodshit #herestosummer",
 			"nickname" : "Jessica"
 		},
 		{
-			"id" : "7",
+			"id" : "2",
 			"group" : "b",
 			"src" : "photo-7.jpg",
 			"comment" : "@tattedcat outty #xmr800 #thewidow with #tricleds halos and @rigidindustriesoffical cubes. Making everything red at @muddaddicts #missinsleepgoindeep last night. #nutznrutz #outlaws #canampowa #xmr #xmr800 #covingtonpike #canam #outlander #catvos #yeti #yeticoolers #goingdeep #seeingred",
 			"nickname" : "nutznrutz"
 		},
 		{
-			"id" : "8",
+			"id" : "3",
 			"group" : "b",
 			"src" : "photo-8.jpg",
 			"comment" : "Friday. #lifted #jackdaniels #jeepfreaks #manshit #beerready #sundayfunday #realestateinvesting #lift #beerme #flexfriday #yeticoolers #bodybuilding #followback #4x4 #jeepfreak #mindset #me #wealth #jeepwrangler #millionaire #jeepin #crossfit #whiskey #beer #yeti #offroad #followme #realestate",
 			"nickname" : "Chase Lovett"
 		},
 		{
-			"id" : "9",
+			"id" : "4",
 			"group" : "b",
 			"src" : "photo-9.jpg",
 			"comment" : "@tristan_bach_ with a 30in northern pike! Keep sending in your pics!! #fishingporn #gunporn #setthathook #seewhatsoutthere #costa #pewpewpew #fish #rodsandreels #bass #lunker #floridalife #saltlife #yeticoolers #shearwater #fishtheflats #hog #fishingfun #columbia #tagsforlikes #saltwater #beaches #tampa #florida #truckporn #basstobillfish #snook #redfish #largemouthbass follow our partners @gone__fishing_ @everything.usa @straightup__outdoors @mtohuntfish SEND IN YOUR PICS BIG OR SMALL",
 			"nickname" : "BTB And Everthing In Between™"
 		},
 		{
-			"id" : "10",
+			"id" : "5",
 			"group" : "b",
 			"src" : "photo-10.jpg",
 			"comment" : "It's going to be a long day. Let's get this show on the road. :) #baileysirishcream #gevalia #keurig #coffee #javajolt #crunkjuice #eno #yeti #enonation #yeticoolers",
 			"nickname" : "Jenni"
 		},
 		{
-			"id" : "11",
+			"id" : "1",
 			"group" : "c",
 			"src" : "photo-11.jpg",
 			"comment" : "Adventures #yeticoolers #builtforthewild @yeticoolers instagram.com/p/3URYV6AJGe/",
 			"nickname" : "Ryan Crook @CrookArmy"
 		},
 		{
-			"id" : "12",
+			"id" : "2",
 			"group" : "c",
 			"src" : "photo-12.jpg",
 			"comment" : "#packin #BuiltForTheWild #SeeWhatsOutThere #GriesRanch #MemorialWeekend #YETI instagram.com/p/3HmB5WTIG0/",
 			"nickname" : "Aaryn Williams @Aaryn_Williams"
 		},
 		{
-			"id" : "13",
+			"id" : "3",
 			"group" : "c",
 			"src" : "photo-13.jpg",
 			"comment" : "First time takin my other babe out and doing some catfishin on lake Palastine #builtforthewild @YetiCoolers pic.twitter.com/cwlCJNR2TC",
 			"nickname" : "Aaron Johnson @thatTXguyaaron"
 		},
 		{
-			"id" : "14",
+			"id" : "4",
 			"group" : "c",
 			"src" : "photo-14.jpg",
 			"comment" : "Great cooler on my flats boat will get 200+ days a year on the water and doubles as customer seat and casting deck !",
 			"nickname" : "Cappymomo"
 		},
 		{
-			"id" : "15",
+			"id" : "5",
 			"group" : "c",
 			"src" : "photo-15.jpg",
 			"comment" : "Our @yeticoolers kept all our food and beer cold for 4 days in the hot desert last weekend. There is nothing better. #BuiltForTheWild #YETIcoolers...",
 			"nickname" : "Shannon and Mike"
 		},
 		{
-			"id" : "16",
+			"id" : "1",
 			"group" : "d",
 			"src" : "photo-16.jpg",
 			"comment" : "It's not just #Builtforthewild it's also #builtforagoodtime #Yeti",
 			"nickname" : "Dustin Allen"
 		},
 		{
-			"id" : "17",
+			"id" : "2",
 			"group" : "d",
 			"src" : "photo-17.jpg",
 			"comment" : "#Habitat work on a nice, warm day at the #FlyingJ. The @polarisorv #Polaris #Ranger and my @yeticoolers #builtforthewild #tundra45 are making the day easier! #birdhunting #quail #pheasants #upland",
 			"nickname" : "Flying J Ranch and Hunt Club"
 		},
 		{
-			"id" : "18",
+			"id" : "3",
 			"group" : "d",
 			"src" : "photo-18.jpg",
 			"comment" : "grueneoutfittersNew shipment of @yeticoolers and accessories!! #seafoamyeti #yeti #builtforthewild #YETIColster #yeticoolers #YETIICE #yetitank #grueneoutfitters #tx",
 			"nickname" : "Gruene Outfitters"
 		},
 		{
-			"id" : "19",
+			"id" : "4",
 			"group" : "d",
 			"src" : "photo-19.jpg",
 			"comment" : "And the Yeti obsession continues... #yeticoolers #yeti #grueneoutfitters #gruenetx #tx #builtforthewild #getoutside",
 			"nickname" : "Gruene Outfitters"
 		},
 		{
-			"id" : "20",
+			"id" : "5",
 			"group" : "d",
 			"src" : "photo-20.jpg",
 			"comment" : "#Yeti #YetiCoolers #BuiltForTheWild #cowboycauldron #YetiHopper",
--- game.php
+++ game.php
@@ -28,13 +28,19 @@
         <![endif]-->
         <div class="content">
             <?php
+                echo '<div class="group-a">';
                 foreach($json[photo] as $p) {
                     if($p[group] == 'a') {
                         echo '
-                            <img src="data/photos/'.$p[src].'" id="'.$p[id].'" class="photo"/> 
+                            <div id="carousel-'.$p[id].'" class="carousel">
+                                <div>reject</div>
+                                <div class="photo"><img src="data/photos/'.$p[src].'" id="'.$p[id].'"/></div>
+                                <div>approve</div>
+                            </div>
                         ';
                     }
                 }
+                echo '</div>';
             ?>
         </div>
 
@@ -48,8 +54,29 @@
         <script src="js/main.js"></script>
         <script>
         $(document).ready(function(){
-          $('.content').slick({
-            setting-name: setting-value
+          $('.carousel').slick({
+            initialSlide: 1
+          });
+          // Show first carousel
+          $('#carousel-1').show();
+          $('.carousel').on('swipe', function(event, slick, direction) {
+            if (direction === 'left') {
+                // TODO: write a rejection
+                console.log(direction);
+            }
+            else if (direction === 'right') {
+                // TODO: write an approval
+                console.log(direction);
+            }
+            var carouselIdStr = $(this).attr('id');
+            var carouselIdInt = parseInt(carouselIdStr.substring(carouselIdStr.length - 1));
+            if (carouselIdInt <= 5) {
+                $('#carousel-' + carouselIdInt).hide();
+                $('#carousel-' + (carouselIdInt + 1)).show();
+            }
+            else {
+                // TODO: show thank you page
+            }
           });
         });
         </script>
