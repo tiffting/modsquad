108630913c80905885717da9d7e705ec701db629
Tiff Ting <tiff.ting@gmail.com>
2015-06-24 21:32:12 -0500

    Dynamically populate results page

--- results.php
+++ results.php
@@ -25,11 +25,11 @@
                <ol class="tally">
                     <li>
                         <small>Rejected</small>
-                        <h2 class="rejected">2</h2>                       
+                        <h2 class="rejected"></h2>                       
                     </li>
                     <li>
                         <small>Approved</small>
-                        <h2 class="approved">4</h2> 
+                        <h2 class="approved"></h2> 
                     </li>
                </ol>
             </div>
@@ -38,7 +38,7 @@
 
         </div>
 
-        <div id="photo-template">
+        <div id="photo-template" class="template">
             <div class="section clearfix">
                 <div class="col col-narrow">
                     <div class="inside">
@@ -57,28 +57,20 @@
                 </div>
                 <div class="col col-wide">
                     <div class="inside">
-                        <h2 class="SOMETHING_STATUSY">SOMETHING_STATUSY</h2>
+                        <h2>SOMETHING_STATUSY</h2>
                         <ol>
-                            <li class="bar SOMETHING_STATUSY">
-                                <div></div>
-                                <div class="percentage" style="width: SOMETHING_PERCENT_APPROVEDY"></div>                            
-                            </li>
-                            <li class="bar SOMETHING_STATUSY">
-                                <div></div>
-                                <div class="percentage" style="width: SOMETHING_PERCENT_APPROVEDY"></div>                            
-                            </li>
-                            <li class="bar SOMETHING_STATUSY">
-                                <div></div>
-                                <div class="percentage" style="width: SOMETHING_PERCENT_APPROVEDY"></div>                            
-                            </li>
-                            <li class="bar SOMETHING_STATUSY">
-                                <div></div>
-                                <div class="percentage" style="width: SOMETHING_PERCENT_APPROVEDY"></div>                            
-                            </li>
+                            <!-- Where approval rating graphs go -->
                         </ol>
                     </div>
                 </div>
             </div>
+        </div>
+
+        <div id="graph-template" class="template">
+            <li class="bar">
+                <div></div>
+                <div class="percentage"></div>                            
+            </li>
         </div>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
@@ -88,44 +80,60 @@
         <script src="js/main.js"></script>
         <script>
             $(document).ready(function(){
-                $.ajax({
+                $.when(getResponses(), getPhotos()).then(function(responsesAjax, photosAjax) {
+                    var responsesJson = validizeIntoJson(responsesAjax[0]);
+                    generateReport(responsesJson, photosAjax[0]);
+                })
+            });
+            var getResponses = function() {
+                return $.ajax({
                     method: 'GET',
                     url: 'data/modResponses.js',
                     dataType: 'text',
-                    success: function(responsesRaw) {
-                        generateReport(responsesRaw);
-                    }
                 });
-            });
-            var generateReport = function(body) {
-                // Variablize DOM elements
-                var $results = $('.results');
-                var $photoTemplate = $('#photo-template').children();
+            };
+            var validizeIntoJson = function(str) {
                 // Create JSON object from modResponses.js with valid JSON wrapper
                 var reTrailingComma = /,$/;
-                var validBody = body.replace(reTrailingComma, '');
-                var contentStr = '{\n\t\"responses\": [' + validBody + '\n\t]\n}';
+                var validBody = str.replace(reTrailingComma, '');
+                var contentStr = '{\n\t\"response\": [' + validBody + '\n\t]\n}';
                 var contentJson = JSON.parse(contentStr);
-                var responses = contentJson.responses;
+                return contentJson;
+            }
+            var getPhotos = function() {
+                return $.getJSON('data/photos.js');
+            };
+            var generateReport = function(responses, photos) {
+                // Variablize DOM elements
+                var $results = $('.results');
+                var photoTemplate = $('#photo-template').html();
+                var graphTemplate = $('#graph-template').html();
+                // Initialize approved and rejected counters
+                var approvedCount = 0;
+                var rejectedCount = 0;
                 // Aggregate approval per photoId+testId(current) combo
                     // e.g., photoId: 21, testId: 3
                     // totalResponses: 100, approvedCount: 75
-                var responsesByPhoto = _.groupBy(responses, 'photoId');
+                var responsesByPhoto = _.groupBy(responses.response, 'photoId');
                 var responsesByPhotoArray = _.map(responsesByPhoto);
                 var photoCount = _.keys(responsesByPhoto).length;
                 var totalResponsesByPhoto = new Array(photoCount);
                 var approvedResponsesByPhoto = new Array(photoCount);
-                // Create multidimensional part to store results per test
                 var totalTests = 4;
                 for (var i = 0; i < photoCount; i++) {
+                    var approvalRating = new Array(totalTests);
+                    var approvedOverall = true;
+                    var strApproved = 'approved';
+                    var strRejected = 'rejected';
+                    // Create multidimensional part to store results per test
                     totalResponsesByPhoto[i] = [' ', ' ', ' ', ' '];
                     approvedResponsesByPhoto[i] = [' ', ' ', ' ', ' '];
-                    $results.append($photoTemplate).html();
+                    // Manipulate DOM
+                    $results.append(photoTemplate);
                     var $thisPhoto = $results.find('.section.clearfix').eq(i);
-                    $thisPhoto.find('img').attr('src', 'data/photos/photo-' + responsesByPhotoArray[0][0].photoId + '.jpg');
-                    // TODO: need to read photos.js for the comment and nickname, ideally for the photo src, too (above).
-                    // $thisPhoto.find('.comment').
-                    // debugger
+                    $thisPhoto.find('img').attr('src', 'data/photos/' + photos.photo[i].src);
+                    $thisPhoto.find('.comment').text(photos.photo[i].comment);
+                    $thisPhoto.find('.nickname').text(photos.photo[i].nickname);
                     var testResponses = _.map(_.groupBy(responsesByPhotoArray[i], 'testId'));
                     for (var j = 0; j < testResponses.length; j++) {
                         // Total responses = length of testResponses[j]
@@ -140,13 +148,32 @@
                         else {
                             approvedResponsesByPhoto[i][j] = 0;
                         }
+                        // Calculate approval rates per test
+                        approvalRating[j] = approvedResponsesByPhoto[i][j] / totalResponsesByPhoto[i][j];
+                        $thisPhoto.find('ol').append(graphTemplate);
+                        var $thisTest = $thisPhoto.find('li').eq(j);
+                        if (approvalRating[j] > 0.5) {
+                            $thisTest.addClass(strApproved);
+                        }
+                        else {
+                            $thisTest.addClass(strRejected);
+                            approvedOverall = false;
+                        }
+                        $thisTest.find('.percentage').css('width', Math.round(approvalRating[j] * 100) + '%');
                     }
+                    // Was photo was approved across all tests?
+                    if (approvedOverall) {
+                        $thisPhoto.find('h2').addClass(strApproved).text(strApproved);
+                        approvedCount++;
+                    }
+                    else {
+                        $thisPhoto.find('h2').addClass(strRejected).text(strRejected);
+                        rejectedCount++;
+                    }
+                    
                 }
-                console.log('approved responses');
-                console.log(approvedResponsesByPhoto);
-                console.log('==================');
-                console.log('total responses');
-                console.log(totalResponsesByPhoto);
+                $results.find('.tally').find('.'+strApproved).text(approvedCount);
+                $results.find('.tally').find('.'+strRejected).text(rejectedCount);
             };
         </script>
     </body>
